9909914f69399050ab6fdcd8e298838e14778932
Sebastian Erives <serivesmejia@deltacv.org>
2026-03-12 11:21:11 -0600

    Change adapter to cloudfare

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 import remarkSlug from 'remark-slug'; // <-- 1. IMPORT
@@ -16,7 +16,7 @@ const config = {
 		]
 	})
 	],
-	kit: { // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+	kit: { // adapter-auto only supports some environments, see https://sve	lte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
